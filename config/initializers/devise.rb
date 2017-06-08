457ba9ec1eba8d117bc01fcb440df3b05eab16a8
krasno86 <krasno86@mail.ru>
2017-06-08 17:38:20 +0300

    Add to heroku,need fix friends

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_CONFIG_APP_SECRET'],
-                  callback_url: "http://localhost:3000/users/auth/facebook/callback",
+                  callback_url: "https://powerful-depths-72874.herokuapp.com/users/auth/facebook/callback",
                   scope: 'email,user_birthday,user_friends,public_profile,user_photos,publish_actions,user_likes,user_posts,user_about_me, user_location, user_website, user_hometown'
   # config.mailer = 'Devise::Mailer'
 
--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -3,7 +3,7 @@ Koala.configure do |config|
   config.app_access_token = ENV['FB_APP_ACCESS_TOKEN']
   config.app_id = ENV['FB_APP_ID']
   config.app_secret = ENV['FB_CONFIG_APP_SECRET']
-  config.api_version = 'v2.3'
+  # config.api_version = 'v2.3'
   # See Koala::Configuration for more options, including details on how to send requests through
   # your own proxy servers.
 end
